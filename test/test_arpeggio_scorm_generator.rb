59298bdf3f3822371986b265407229781c20c66f
Matt Fawcett <fawcett@viddler.com>
2015-07-03 10:13:25 +0100

    Switch to minitest to make having a setup method with separate tests easier

--- test/test_arpeggio_scorm_generator.rb
+++ test/test_arpeggio_scorm_generator.rb
@@ -1,32 +1,38 @@
 require_relative 'test_helper'
 
-class TestArpeggioScormGenerator < Test::Unit::TestCase
-  def test_generation
+class TestArpeggioScormGenerator < Minitest::Test
+  def setup
     options = {
       width: '100%',
       videoId: 'abc123'
     }
-    temp_file = Tempfile.new('built.zip')
-    generator = ArpeggioScormGenerator::Generator.new(options, temp_file)
+    @temp_file = Tempfile.new('built.zip')
+    generator = ArpeggioScormGenerator::Generator.new(options, @temp_file)
     generator.generate
+  end
 
 
-    Zip::File.open(temp_file.path) do |zip_file|
-      # ensure the file has top level items included
+  def test_should_include_top_level_files
+    Zip::File.open(@temp_file.path) do |zip_file|
       expected = File.read(File.dirname(__FILE__) + '/../template/xml.xsd')
       actual   = zip_file.glob('arpeggio_scorm_package/xml.xsd').first.get_input_stream.read
       assert_equal expected, actual
+    end
+  end
 
-      # ensure sub directory files are included
+  def test_should_include_sub_folders
+    Zip::File.open(@temp_file.path) do |zip_file|
       expected = File.open(File.dirname(__FILE__) + '/../template/content/util/APIWrapper.js', 'rb').read
       actual   = zip_file.glob('arpeggio_scorm_package/content/util/APIWrapper.js').first.get_input_stream.read
       assert_equal expected, actual
+    end
+  end
 
-      # ensure we have dynamically generated config.json
+  def test_should_write_dynamically_generated_config_json
+    Zip::File.open(@temp_file.path) do |zip_file|
       expected = File.open(File.dirname(__FILE__) + '/expected_config.json', 'rb').read
       actual   = zip_file.glob('arpeggio_scorm_package/config.json').first.get_input_stream.read
       assert_equal expected, actual
     end
-
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,3 @@
+gem 'minitest'
+require 'minitest/autorun'
 require 'arpeggio_scorm_generator'
-require 'test/unit'
